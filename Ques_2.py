9b3ef2ceaddb03fb6b2fcd3e7103a1e9bd0b7d50
jaiswalrahul2427 <jaiswalrahul2427@gamil.com>
2025-12-01 11:31:15 +0530

    second task modified

--- Ques_2.py
+++ Ques_2.py
@@ -1,24 +1,37 @@
-#Show bar Graphs representing the departure time & Arrival time.
-
-import pandas as pd
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+data = pd.read_csv("airlines_flights_data.csv")
+data.drop(columns='index', inplace=True)
 
-data=pd.read_csv("airlines_flights_data.csv")
-data.drop(columns='index', inplace= True)
+# Departure time counts
+dep_counts = data['departure_time'].value_counts()
+print(dep_counts)
 
-# print(data)
+# Arrival time counts
+arr_counts = data['arrival_time'].value_counts()
+print(arr_counts)
 
-# data['departure_time'].value_counts()
-# plt.xlabel("departure_time")
-# plt.ylabel("arrival_time")
-# plt.show()
-departure_data=data['departure_time'].value_counts(ascending=True)
-arrival_data=data['arrival_time'].value_counts(ascending=True)
- 
-plt.bar(departure_data.index,departure_data.values)
+# Plotting
+plt.figure(figsize=(16,4))
+
+# ----- Departure Time -----
+plt.subplot(1,2,1)
+plt.bar(dep_counts.index, dep_counts.values)
+plt.title("Departure Time Counts")
+plt.xlabel("Departure Time")
+plt.ylabel("Frequency")
+plt.xticks(rotation=45)
+
+# ----- Arrival Time -----
+plt.subplot(1,2,2)
+plt.bar(arr_counts.index, arr_counts.values)
+plt.title("Arrival Time Counts")
+plt.xlabel("Arrival Time")
+plt.ylabel("Frequency")
+plt.xticks(rotation=45)
+
+plt.tight_layout()
 plt.show()
-21.24
